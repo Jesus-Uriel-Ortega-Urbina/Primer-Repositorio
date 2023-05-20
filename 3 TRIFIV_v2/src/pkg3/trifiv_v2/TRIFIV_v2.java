888962c318126be82c68f4cbadf1559e2bf2c180
Uriel <urielmaseratu@gmail.com>
2023-05-19 20:06:30 -0600

    Agregamos trifiv_v2

--- 3 TRIFIV_v2/src/pkg3/trifiv_v2/TRIFIV_v2.java
+++ 3 TRIFIV_v2/src/pkg3/trifiv_v2/TRIFIV_v2.java
@@ -1,4 +1,4 @@
-
+//version 2 modificar el ultimo static
 package pkg3.trifiv_v2;
 
 import java.io.BufferedReader;
@@ -11,42 +11,31 @@ public class TRIFIV_v2 {
     
     public static void main(String[] args) throws IOException {
   
-        
-     
-        //Declaracion de variables 
+
         BufferedReader bufEntrada = new BufferedReader(new InputStreamReader(System.in));  
-        //bufferedReder nos manda cuales imprtaciones son requeridas 
+        //bufferedReder nos manda cuales imprtaciones son requeridas uwu
         int numero; 
         float mult3;
         float mult5;
-        //Entrada de datos
+        
         System.out .println("Dame el numero: ");
+        
         numero = Integer.parseInt(bufEntrada.readLine());
-        //Procesar datos
-        
-        mult3 = numero%3;
-        mult5 = numero%5;
-        
-        System.out.println(retornaMultiplo(numero));
-        
-    }
-    
-    public static String retornaMultiplo (int num)
-            
-    { 
-        if(num %3==0 && num%5 ==0)
+
+       
+        if(numero %3==0 && numero%5 ==0)
         {
             System.out.println("TRIFIV");
         }
-        else if (num%3==0)
+        else if (numero%3==0)
         {
            System.out.println("TRI");
         }
-        else if (num%5==0)
+        else if (numero%5==0)
         {
             System.out.println("FIV");
   
         }
-        return  "no es multiplo";
+       
     }
 }
