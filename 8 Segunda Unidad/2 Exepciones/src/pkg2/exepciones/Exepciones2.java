a7b7ec4605a3c5432374c62041a2c6b59ae31de7
Uriel <urielmaseratu@gmail.com>
2023-07-01 22:20:46 -0600

    Agregamos Codigo 2

---
+++ 8 Segunda Unidad/2 Exepciones/src/pkg2/exepciones/Exepciones2.java
@@ -0,0 +1,33 @@
+
+package pkg2.exepciones;
+
+import javax.swing.JOptionPane;
+
+
+public class Exepciones2 {
+
+
+    public static void main(String[] args) {
+        
+        int x, y;
+        
+        try {
+        
+        x= Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa numero 1:", "Solicita Datos", 3));
+        y= Integer.parseInt(JOptionPane.showInputDialog(null, "Ingresa numero 2:", "Solicita Datos", 3));
+        
+        JOptionPane.showMessageDialog(null, "La Divicion de "+ x + "/" + y + " es" + (x/y), "Mostrar resultado", 1);
+
+        
+    } catch (NumberFormatException n) {
+        
+            System.out.println(n.getMessage() + " No le sabes, eso no es un numero");
+            
+        } catch (ArithmeticException a) {
+        
+            System.out.println(a.getMessage() + " No se puede dividir entre 0");
+        
+        }
+    } 
+}
+
