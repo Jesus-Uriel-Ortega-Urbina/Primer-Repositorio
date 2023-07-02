2b3656495f3cf49bb914817ed113d47e538d303f
Uriel <urielmaseratu@gmail.com>
2023-07-01 22:21:45 -0600

    Agregamos codigo 3

---
+++ 8 Segunda Unidad/3 Exepciones3/src/pkg3/exepciones3/Exepciones3.java
@@ -0,0 +1,27 @@
+
+package pkg3.exepciones3;
+
+import javax.swing.JOptionPane;
+
+
+public class Exepciones3 {
+
+
+    public static void main(String[] args) {
+        
+   try {
+       float equis = 5/0;
+       System.out.println("Equis = " + equis);
+       
+   } catch (ArithmeticException e) {
+        System.out.println("Error al dividir entre 0");
+        
+   } finally { //siempre se va a ejecutar haya error o no
+       
+       System.out.println("A pesar de todo se ejecuta Finaly");
+      }
+        
+    }
+        
+  
+ }
